3e5ecddcf54b2b924f6634c432f764c5b3c38816
Alex Ben <alexander.bendel@gmail.com>
2021-08-06 00:12:49 +0200

    remove useless import

--- src/app/uploaded-deals/services/uploaded-deals.service.ts
+++ src/app/uploaded-deals/services/uploaded-deals.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { delay, filter, tap } from 'rxjs/operators';
-import { __makeTemplateObject } from 'tslib';
 import { UploadedDeal } from '../models/uploaded-deal';
 
 @Injectable({
